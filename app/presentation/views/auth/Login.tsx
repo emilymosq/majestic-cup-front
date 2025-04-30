309f2fe5911925a2438de13244171a492f8c69c0
danimor99 <danimor1999@gmail.com>
2025-04-30 11:50:53 +0200

    Subiendo pantalla login

--- app/presentation/views/auth/Login.tsx
+++ app/presentation/views/auth/Login.tsx
@@ -1,16 +1,54 @@
 import React from "react";
-import {Button, Text, View} from "react-native";
+import {Text, View, Image} from "react-native";
+import FormInput from "../../components/FormInput";
+import styles from "./StylesLogin";
 import {PropsStackNavigation} from "../../interfaces/StackNav";
+import {LoginButton} from "../../components/LoginButton";
 
-const Login = ({navigation, route}: PropsStackNavigation) => {
-    return(
-        <View style={{alignItems: 'center', justifyContent: 'center', flex: 1}}>
-            <Text>Login</Text>
-            <Button title={"Entrar"} onPress={()=>{
-                navigation.navigate('BottomTabNavigator')
-            }}/>
+export function Login({navigation, route}: PropsStackNavigation) {
+
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoContainer}>
+                <Image
+                    style={styles.logo}
+                    //source={require('../../../../assets/')}
+                ></Image>
+            </View>
+            <Text style={styles.title}>¡Bienvenido! </Text>
+            <Text style={styles.subTitle}>Inicia sesión con tu cuenta</Text>
+
+            <View style={styles.formContainer}>
+
+                <FormInput
+                    image={null}
+                    text={"Correo eletrónico"}
+                    placeholder={""}
+                    keyboardType="default"
+                    secureTextEntry={false}
+                    editable={true}
+                    onPressFormInterface={(text) => setEmail(text)}
+
+                ></FormInput>
+                <FormInput
+                    image={null}
+                    text={"Contraseña"}
+                    placeholder={""}
+                    keyboardType="default"
+                    secureTextEntry={true}
+                    editable={true}
+                    onPressFormInterface={(text) => setPassword(text)}
+
+                ></FormInput>
+                <View style={styles.buttonContainer}>
+                    <LoginButton onPress={() => { navigation.navigate('BottomTabNavigator')}} text={"Entrar"}/>
+                </View>
+            </View>
         </View>
-    )
+    );
 }
 
 export default Login;
